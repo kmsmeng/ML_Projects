dc3329c394c872600b4670608725fb4c27a3a747
Kiran Malla <kiranmugu@gmail.com>
2025-05-26 17:51:07 -0500

    obj

--- YOLO_Obj_Detection/train.py
+++ YOLO_Obj_Detection/train.py
@@ -1,6 +1,17 @@
 import torch
+import torchvision.transforms as transforms
 import torch.optim as optim
+import torchvision.transforms.functional as FT
 from tqdm import tqdm
+from model import YOLOV1
+from dataset import VOCDataset
+from utils import (
+    intersection_over_union,
+    non_max_suppression,
+    mean_average_precision
+)
 
-for x in range(60):
-    print(x)
+from loss import YoloLoss
+
+for x in tqdm(range(60)):
+    pass
